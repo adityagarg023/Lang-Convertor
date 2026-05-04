ffd356e897c43e5d72e13f962475c27b59a396e1
Aditya Garg <140079562+adityagarg023@users.noreply.github.com>
2026-05-04 17:06:41 +0530

    Add files via upload

---
+++ components/header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <div className='flex items-center justify-between p-4 text-white'>
+            <h1 className="jet text-3xl">Lang Convertor</h1>
+        </div>
+    )
+}
+export default Header
